cadff2a94f3e872d4070412b53337e7a2ba855ee
aibayanyu20 <aibayanyu@qq.com>
2025-11-22 09:59:11 +0800

    chore: add radio demo

---
+++ playground/src/pages/radio/demo/radiobutton.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { shallowRef } from 'vue'
+
+const value = shallowRef()
+</script>
+
+<template>
+  <a-flex vertical gap="middle">
+    <a-radio-group v-model:value="value" default-value="a">
+      <a-radio-button value="a">
+        Hangzhou
+      </a-radio-button>
+      <a-radio-button value="b">
+        Shanghai
+      </a-radio-button>
+      <a-radio-button value="c">
+        Beijing
+      </a-radio-button>
+      <a-radio-button value="d">
+        Chengdu
+      </a-radio-button>
+    </a-radio-group>
+    <a-radio-group v-model:value="value" default-value="a">
+      <a-radio-button value="a">
+        Hangzhou
+      </a-radio-button>
+      <a-radio-button value="b" disabled>
+        Shanghai
+      </a-radio-button>
+      <a-radio-button value="c">
+        Beijing
+      </a-radio-button>
+      <a-radio-button value="d">
+        Chengdu
+      </a-radio-button>
+    </a-radio-group>
+    <a-radio-group v-model:value="value" disabled default-value="a">
+      <a-radio-button value="a">
+        Hangzhou
+      </a-radio-button>
+      <a-radio-button value="b">
+        Shanghai
+      </a-radio-button>
+      <a-radio-button value="c">
+        Beijing
+      </a-radio-button>
+      <a-radio-button value="d">
+        Chengdu
+      </a-radio-button>
+    </a-radio-group>
+  </a-flex>
+</template>
---
+++ playground/src/pages/radio/demo/radiogroup-block.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { CheckboxOptionType } from 'antdv-next'
+import { shallowRef } from 'vue'
+
+const options: CheckboxOptionType[] = [
+  { label: 'Apple', value: 'Apple' },
+  { label: 'Pear', value: 'Pear' },
+  { label: 'Orange', value: 'Orange' },
+]
+const value = shallowRef()
+</script>
+
+<template>
+  <a-flex vertical gap="middle">
+    <a-radio-group v-model:value="value" block :options="options" default-value="Apple" />
+    <a-radio-group
+      v-model:value="value"
+      block
+      :options="options"
+      default-value="Apple"
+      option-type="button"
+      button-style="solid"
+    />
+    <a-radio-group
+      v-model:value="value"
+      block
+      :options="options" default-value="Pear" option-type="button"
+    />
+  </a-flex>
+</template>
